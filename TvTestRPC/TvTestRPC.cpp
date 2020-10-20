fb301ddbdf4843d3b9c6ba052626d43df269ca77
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2020-10-20 23:22:16 +0900

    32bit版で「TVTGetVersion()関数のアドレスを取得できません。」とエラーが出るのを修正！

--- TvTestRPC/TvTestRPC.cpp
+++ TvTestRPC/TvTestRPC.cpp
@@ -14,11 +14,6 @@ public:
 	void UpdateState();
 	time_t SystemTime2Timet(const SYSTEMTIME&);
 
-	DWORD GetVersion() override
-	{
-		return TVTEST_PLUGIN_VERSION_(0,0,14);
-	}
-
 	bool GetPluginInfo(TVTest::PluginInfo *pInfo) override
 	{
 		pInfo->Type = TVTest::PLUGIN_TYPE_NORMAL;
