1959a1cb88ddf1b84d5803e5dc06c23938d487a0
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2020-10-21 17:28:46 +0900

    ストリームからチャンネル名を取得するように設定から変更できるように。

--- TvTestRPC/TvTestRPC.cpp
+++ TvTestRPC/TvTestRPC.cpp
@@ -3,32 +3,44 @@
 
 #include "stdafx.h"
 #include <time.h>
+#include <shlwapi.h>
+#include "resource.h"
+#pragma comment(lib,"shlwapi.lib")
+
 class CMyPlugin : public TVTest::CTVTestPlugin
 {
 	DiscordEventHandlers handlers;
-	static LRESULT CALLBACK EventCallback(UINT Event, LPARAM lParam1, LPARAM lParam2, void *pClientData);
+	static LRESULT CALLBACK EventCallback(UINT Event, LPARAM lParam1, LPARAM lParam2, void* pClientData);
+	static INT_PTR CALLBACK SettingsDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam, void* pClientData);
+	bool ShowDialog(HWND hwndOwner);
+	POINT m_SettingsDialogPos;
+	TCHAR m_szIniFileName[MAX_PATH];
+	bool conf_mode = false;
+	bool conf_isFinalized = false;
+	bool InitSettings();
+	bool pluginState = false;
+	void InitDiscord();
+	void UpdateState();
+	static const int DEFAULT_POS = INT_MIN;
 
 public:
-	bool pluginState;
-	void discordInit();
-	void UpdateState();
 	time_t SystemTime2Timet(const SYSTEMTIME&);
 
-	bool GetPluginInfo(TVTest::PluginInfo *pInfo) override
+	bool GetPluginInfo(TVTest::PluginInfo* pInfo) override
 	{
 		pInfo->Type = TVTest::PLUGIN_TYPE_NORMAL;
-		pInfo->Flags = 0;
+		pInfo->Flags = TVTest::PLUGIN_FLAG_HASSETTINGS;
 		pInfo->pszPluginName = L"TvTest RPC";
 		pInfo->pszCopyright = L"(c) 2019-2020 noriokun4649";
 		pInfo->pszDescription = L"DiscordRPCをTvTestで実現します。Discordで視聴中のチャンネルなどの情報が通知されます。";
-		return true; 
+		return true;
 	}
 
 	bool Initialize() override
 	{
-		discordInit();
+		InitDiscord();
 		m_pApp->SetEventCallback(EventCallback, this);
-		return true; 
+		return true;
 	}
 	bool Finalize() override
 	{
@@ -37,16 +49,24 @@ public:
 	}
 };
 
-void CMyPlugin::discordInit()
+
+bool CMyPlugin::InitSettings() {
+	::GetModuleFileName(g_hinstDLL, m_szIniFileName, MAX_PATH);
+	::PathRenameExtension(m_szIniFileName, TEXT(".ini"));
+	conf_mode = ::GetPrivateProfileInt(TEXT("Settings"), TEXT("Mode"), conf_mode, m_szIniFileName) != 0;
+	m_pApp->AddLog(m_szIniFileName);
+	conf_isFinalized = true;
+	return true;
+}
+
+void CMyPlugin::InitDiscord()
 {
 	Discord_Initialize("577065126084214816", &handlers, 1, NULL);
 }
 
 void CMyPlugin::UpdateState()
 {
-	TVTest::ChannelInfo ChannelInfo;
 	TVTest::ProgramInfo Info;
-	ChannelInfo.Size = sizeof(ChannelInfo);
 	Info.Size = sizeof(Info);
 	wchar_t eventName[128];
 	wchar_t eventText[128];
@@ -61,15 +81,25 @@ void CMyPlugin::UpdateState()
 	std::string eventNamed;
 	time_t start;
 	time_t end;
-	
+
 	if (m_pApp->GetCurrentProgramInfo(&Info)) {
 		eventNamed = wide_to_utf8(Info.pszEventName);
 		end = SystemTime2Timet(Info.StartTime) + Info.Duration;
 		start = SystemTime2Timet(Info.StartTime);
 	}
-	if (m_pApp->GetCurrentChannelInfo(&ChannelInfo) ) {
-		m_pApp->AddLog(ChannelInfo.szChannelName);
-		channelName =  wide_to_utf8(ChannelInfo.szChannelName);
+	if (!conf_mode) {
+		TVTest::ServiceInfo Service;
+		Service.Size = sizeof(Service);
+		if (m_pApp->GetServiceInfo(0, &Service)) {
+			channelName = wide_to_utf8(Service.szServiceName);
+		}
+	}
+	else {
+		TVTest::ChannelInfo ChannelInfo;
+		ChannelInfo.Size = sizeof(ChannelInfo);
+		if (m_pApp->GetCurrentChannelInfo(&ChannelInfo)) {
+			channelName = wide_to_utf8(ChannelInfo.szChannelName);
+		}
 	}
 	DiscordRichPresence discordPresence;
 	memset(&discordPresence, 0, sizeof(discordPresence));
@@ -95,19 +125,70 @@ time_t CMyPlugin::SystemTime2Timet(const SYSTEMTIME& st)
 	return mktime(&gm);
 }
 
-TVTest::CTVTestPlugin *CreatePluginClass()
+TVTest::CTVTestPlugin* CreatePluginClass()
 {
 	return new CMyPlugin;
 }
-LRESULT CALLBACK CMyPlugin::EventCallback(UINT Event, LPARAM lParam1, LPARAM lParam2, void *pClientData)
+
+bool CMyPlugin::ShowDialog(HWND hwndOwner) {
+
+	TVTest::ShowDialogInfo Info;
+	Info.Flags = 0;
+	Info.hinst = g_hinstDLL;
+	Info.pszTemplate = MAKEINTRESOURCE(IDD_DIALOG1);
+	Info.pMessageFunc = SettingsDlgProc;
+	Info.pClientData = this;
+	Info.hwndOwner = hwndOwner;
+	if (m_SettingsDialogPos.x != DEFAULT_POS && m_SettingsDialogPos.y != DEFAULT_POS) {
+		Info.Flags |= TVTest::SHOW_DIALOG_FLAG_POSITION;
+		Info.Position = m_SettingsDialogPos;
+	}
+
+	return m_pApp->ShowDialog(&Info) == IDOK;
+}
+
+INT_PTR CALLBACK CMyPlugin::SettingsDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam, void* pClientData) {
+	CMyPlugin* pThis = static_cast<CMyPlugin*>(pClientData);
+	switch (uMsg)
+	{
+	case WM_INITDIALOG: 
+		::CheckDlgButton(hDlg, IDC_CHECK1, pThis->conf_mode ? BST_CHECKED : BST_UNCHECKED);
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)) {
+		case IDOK:
+			pThis->conf_mode = ::IsDlgButtonChecked(hDlg, IDC_CHECK1) == BST_CHECKED;
+			if (pThis->conf_isFinalized) {
+				struct IntString {
+					IntString(int Value) { ::wsprintf(m_szBuffer, TEXT("%d"), Value); }
+					operator LPCTSTR() const { return m_szBuffer; }
+					TCHAR m_szBuffer[16];
+				};
+
+				::WritePrivateProfileString(TEXT("Settings"), TEXT("Mode"), IntString(pThis->conf_mode), pThis->m_szIniFileName);
+			}
+			pThis->UpdateState();
+		case IDCANCEL:
+			::EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+	default:
+		break;
+		return FALSE;
+	}
+	return FALSE;
+}
+
+LRESULT CALLBACK CMyPlugin::EventCallback(UINT Event, LPARAM lParam1, LPARAM lParam2, void* pClientData)
 {
-	CMyPlugin *pThis = static_cast<CMyPlugin *>(pClientData);
+	CMyPlugin* pThis = static_cast<CMyPlugin*>(pClientData);
 	bool fEnable = lParam1 != 0;
 	switch (Event)
 	{
 	case TVTest::EVENT_PLUGINENABLE:
 		if (fEnable) {
 			pThis->UpdateState();
+			pThis->InitSettings();
 		}
 		else
 		{
@@ -121,6 +202,8 @@ LRESULT CALLBACK CMyPlugin::EventCallback(UINT Event, LPARAM lParam1, LPARAM lPa
 	case TVTest::EVENT_SERVICEUPDATE:
 		pThis->UpdateState();
 		return TRUE;
+	case TVTest::EVENT_PLUGINSETTINGS:
+		return pThis->ShowDialog(reinterpret_cast<HWND>(lParam1));
 	}
 	return 0;
 }
