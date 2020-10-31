97de64d932f0906250664e9d132153f76997e175
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2020-11-01 05:33:55 +0900

    フォントサイズをちょっち変更

--- TvTestRPC/resource.h
+++ TvTestRPC/resource.h
@@ -3,14 +3,13 @@
 // TvTestRPC.rc ‚ÅŽg—p
 //
 #define IDD_DIALOG1                     101
-#define IDC_COMBO1                      1003
 #define IDC_CHECK1                      1004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
