dc079d00097eab081ec26649babde3f9fcfd65c0
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2020-11-09 11:42:20 +0900

    設定項目を2つ追加ｰ>リソースファイル

--- TvTestRPC/resource.h
+++ TvTestRPC/resource.h
@@ -4,6 +4,8 @@
 //
 #define IDD_DIALOG1                     101
 #define IDC_CHECK1                      1004
+#define IDC_CHECK2                      1005
+#define IDC_CHECK3                      1006
 
 // Next default values for new objects
 // 
@@ -11,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
